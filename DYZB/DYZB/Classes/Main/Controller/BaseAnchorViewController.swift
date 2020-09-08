a98cead906c6dabf0077b2b085fc6668f2285925
Steven.zhao <865691337@qq.com>
2020-09-08 14:23:28 +0800

    添加扩展

---
+++ DYZB/DYZB/Classes/Main/Controller/BaseAnchorViewController.swift
@@ -0,0 +1,98 @@
+//
+//  BaseAnchorViewController.swift
+//  DYZB
+//
+//  Created by 赵宏图 on 2020/9/8.
+//  Copyright © 2020 赵宏图. All rights reserved.
+//
+
+import UIKit
+
+private let KItemMargin:CGFloat = 10
+let KNormalItemW = (KScreenW - 3 * KItemMargin) / 2
+let KNormalItemH = KNormalItemW * 3 / 4
+let KPrettyItemH = KNormalItemW * 4 / 3
+private let KHeaderViewH:CGFloat = 50
+private let KCycleViewH = KScreenH * 2 / 8
+private let KGameViewH:CGFloat = 90
+
+private let KNormalCellID = "KNormalCellID"
+let KPrettyCellID = "KPrettyCellID"
+private let KHeaderViewID = "KHeaderViewID"
+
+class BaseAnchorViewController: UIViewController {
+    
+    //MARK: - 定义属性
+    var baseVM:BaseViewModel!
+    
+    //MARK: - 懒加载属性
+    lazy var collectionView:UICollectionView = { [unowned self] in
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: KNormalItemW, height: KNormalItemH)
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = KItemMargin
+        layout.headerReferenceSize = CGSize(width: KScreenW, height: KHeaderViewH)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: KItemMargin, bottom: 0, right: KItemMargin)
+        
+        let collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
+        collectionView.backgroundColor = UIColor.white
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UINib(nibName: "CollectionNormalCell", bundle: nil), forCellWithReuseIdentifier: KNormalCellID)
+        collectionView.register(UINib(nibName: "CollectionPrettyCell", bundle: nil), forCellWithReuseIdentifier: KPrettyCellID)
+        collectionView.register(UINib(nibName: "CollectionHeaderView", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: KHeaderViewID)
+        collectionView.autoresizingMask = [.flexibleHeight,.flexibleWidth]
+        return collectionView
+    }()
+    
+    //MARK: - 系统回调
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        
+        loadData()
+    }
+}
+
+//MARK: - 设置UI界面
+extension BaseAnchorViewController
+{
+    @objc func setupUI() {
+        view.addSubview(collectionView)
+    }
+}
+
+//MARK: - 发送网络请求
+extension BaseAnchorViewController
+{
+    @objc func loadData()
+    {}
+}
+
+//MARK: - 遵循UICollectionView的数据源和协议
+extension BaseAnchorViewController:UICollectionViewDataSource,UICollectionViewDelegate
+{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return baseVM.anchorGroups.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return baseVM.anchorGroups[section].anchors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: KNormalCellID, for: indexPath)
+         as! CollectionNormalCell
+        
+        cell.anchor = baseVM.anchorGroups[indexPath.section].anchors[indexPath.item]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let cell = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: KHeaderViewID, for: indexPath) as! CollectionHeaderView
+        cell.group = baseVM.anchorGroups[indexPath.section]
+        return cell
+    }
+}
